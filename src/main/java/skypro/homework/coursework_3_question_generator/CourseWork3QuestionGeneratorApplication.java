8fcf46b7c2ed9aa0a207d0a02e0dba977fd34c9e
zemscky <aleksei.zemscky@yandex.ru>
2022-12-03 15:46:57 +0300

    Initial commit

---
+++ src/main/java/skypro/homework/coursework_3_question_generator/CourseWork3QuestionGeneratorApplication.java
@@ -0,0 +1,13 @@
+package skypro.homework.coursework_3_question_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork3QuestionGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWork3QuestionGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/homework/coursework_3_question_generator/CourseWork3QuestionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.homework.coursework_3_question_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork3QuestionGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
